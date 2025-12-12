68bdbaffa4420d486b5516a4e9588103d13813fd
MDRobiulHassan <robiul110256@gmail.com>
2025-12-12 22:42:03 +0600

    Hibernate

---
+++ Spring-Data-JPA/src/main/java/com/JPA/Spring/Data/JPA/entity/Product.java
@@ -0,0 +1,46 @@
+package com.JPA.Spring.Data.JPA.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(
+        name = "product",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "sku_unique",columnNames = {"sku"}),
+                @UniqueConstraint(name = "title_price_unique",columnNames = {"name","price"})
+        },
+        indexes = {
+                @Index(name = "sku_index",columnList = "sku")
+        }
+)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false,length = 20)
+    private String sku;
+
+    @Column(name="name")
+    private String title;
+
+    private BigDecimal price;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ Spring-Data-JPA/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (price, sku, name)
+VALUES
+    (199.50, 'SKU-1002', 'Mechanical Keyboard'),
+    (59.99, 'SKU-1003', 'Gaming Mouse'),
+    (999.00, 'SKU-1004', '144Hz Monitor');
