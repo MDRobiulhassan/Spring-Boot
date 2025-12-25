450a19d5b41eff7aed39748f884ed34a938c0c69
MDRobiulHassan <robiul110256@gmail.com>
2025-12-25 23:44:30 +0600

    Internal Working of Spring Security

---
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/advice/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.SpringSecurity.advice;
+
+
+import com.example.SpringSecurity.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        ApiError apiError = new ApiError(exception.getLocalizedMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+}
