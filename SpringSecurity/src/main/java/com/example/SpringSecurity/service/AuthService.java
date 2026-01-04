160607da540475c7228096ce185caf79145ff771
MDRobiulHassan <robiul110256@gmail.com>
2026-01-04 21:59:37 +0600

    User Session

--- SpringSecurity/src/main/java/com/example/SpringSecurity/service/AuthService.java
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/service/AuthService.java
@@ -27,6 +27,7 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final jwtService jwtService;
     private final UserService userService;
+    private final SessionService sessionService;
 
     public UserDTO signUp(SignupDTO signupDTO) {
         Optional<User> user = userRepository.findByEmail(signupDTO.getEmail());
@@ -52,11 +53,14 @@ public class AuthService {
         String accessToken = jwtService.generateAccessToken(user);
         String refreshToken = jwtService.generateRefreshToken(user);
 
+        sessionService.generateNewSession(user, refreshToken);
+
         return new LoginResponseDTO(user.getId(),accessToken,refreshToken);
     }
 
     public LoginResponseDTO refreshToken(String refreshToken) {
         Long userId = jwtService.getUserIdByToken(refreshToken);
+        sessionService.validateSession(refreshToken);
         User user = userService.getUserById(userId);
 
         String accessToken = jwtService.generateAccessToken(user);
