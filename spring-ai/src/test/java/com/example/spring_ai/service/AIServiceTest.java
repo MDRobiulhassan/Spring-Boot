e60971e5347ad65ae577c71abfba4581ddbffa51
MDRobiulHassan <robiul110256@gmail.com>
2026-03-03 03:02:59 +0600

    Embedding and Vector Search

--- spring-ai/src/test/java/com/example/spring_ai/service/AIServiceTest.java
+++ spring-ai/src/test/java/com/example/spring_ai/service/AIServiceTest.java
@@ -15,4 +15,31 @@ public class AIServiceTest {
         var joke = aiService.getJoke("Dogs");
         System.out.println(joke);
     }
+
+    @Test
+    public void testEmbedText(){
+        var embed = aiService.getEmbedding("Hello world");
+        System.out.println(embed.length);
+        for (float e : embed) {
+            System.out.print(e + " ");
+        }
+    }
+
+    @Test
+    public void testStoreData(){
+        aiService.ingestDataToVectorStore("This is a big data");
+    }
+
+    @Test
+    public void testStoreDataList(){
+        aiService.ingestDataToVectorStore();
+    }
+
+    @Test
+    public void testSimilaritySearch(){
+        var results = aiService.similaritySearch("Space Movie");
+        for(var doc : results){
+            System.out.println(doc);
+        }
+    }
 }
