607be451164eacd5083b087e32558ca4c8955436
MDRobiulHassan <robiul110256@gmail.com>
2026-06-17 22:23:28 +0600

    Spring Boot Default Cache and Redis Cache

---
+++ caching/src/main/java/com/example/caching/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.caching.repository;
+
+import com.example.caching.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByEmail(String email);
+}
